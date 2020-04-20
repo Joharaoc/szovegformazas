138d69826ce34009d1013c202a0bbfcdf8c129d0
Joharaoc <lilinotes@gmail.com>
2020-04-20 14:15:36 +0200

    make js smaller

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 
-const irasjelek=",;:.!?)";
-
-String.prototype.remove_spacechar = function (char) {
-    var temp=this;
-    var spacechar=' '+char;
-    while (temp.indexOf(spacechar) !== -1) {
-      temp=temp.replace(spacechar, char);
-    }
-    return temp;
-};
-String.prototype.replaceAll = function (a,b) {
-    var temp=this;
-    while (temp.indexOf(a) !== -1) {
-      temp=temp.replace(a, b);
-    }
-    return temp;
-};
-
-
 function lapoz(){
   $('#info').toggle();
   $('#app').toggle();
@@ -35,54 +16,32 @@ $('#clear').on('click', function(e){
 
 });
 
+
 $('#replace').on('click', function(e){
 
-var roots=$('#edit');
-for (var i = 0; i < roots.length; i++) {
-  var root = $(roots[i])
-  root.append(root.find("div > div"))
-}
+  var roots=$('#edit');
 
-var re = new RegExp(/&nbsp;|<br>/, "g");
-var prevTagName="";
-$('#edit').children().each(function() {
-    var $this = $(this);
-        console.log($this, prevTagName);
-    if($this.html().length != 0 ){
-      $('#edit').append(document.createTextNode($this.html().replace(re," ")));
-      $('#edit').append('<br>');
-    }
-    var tagName=$this.prop('tagName');
-    if (tagName != 'BR' ) {
-      console.log(prevTagName, tagName);
-      $this.remove();
-    }
-    prevTagName=tagName;
-});
-
-  var content=$('#edit').html();
-  console.log(content);
-  content=content.replaceAll('<br> <br>','<br>');
-  content=content.replaceAll('--','-');
-  content=content.replaceAll('....','...');
-  content=content.replace(/^ /g,'');
-  content=content.replace(/-/g,' - ');
-  content=content.replace(/\+/g,' + ');
-  content=content.replace(/\*/g,' * ');
-  content=content.replace(/=/g,' = ');
-  content=content.replace(/\(/g,' (');
-  content=content.replace(/\( /g,'(');
-
-
-  for (var i = 0; i < irasjelek.length; i++) {
-    var irasjel = irasjelek.charAt(i);
-    var re = new RegExp('\\'+irasjel,"g");
-    content=content.replace(re,irasjel+" ");
-    content=content.remove_spacechar(irasjel);
+  for (var i = 0; i < roots.length; i++) {
+    var root = $(roots[i])
+    root.append(root.find("div > div"))
   }
-  content=content.replaceAll('  ',' ');
 
-
-  $('#edit').html(content);
+  var html=$('#edit').html()
+  var text = html.replace(/<[^(a)(br)(div)(p)][^>]+>/g, '');
+  text=text.replace(/<div[^>]+>/g, '<br>')
+  text=text.replace(/<p[^>]+>/g, '<br>')
+  text=text.replace(/<br>\s*<br>\s*<br>/gm, '<br><br>')
+  text=text.replace(/(&nbsp;)+/g, ' ')
+  text=text.replace(/ +/g, ' ')
+  text=text.replace(/-+/g, '-')
+  text=text.replace(/\.\.\.+/g, '...')
+  text=text.replace(/ ([\.\?\!\,\:])/g, '$1')
+  text=text.replace(/([\.\?\!\,\:])(?! )/g, '$1 ')
+  text=text.replace(/\? \!/g, '?!')
+  text=text.replace(/\! \?/g, '!?')
+  text=text.replace(/\! \! \!/g, '!!!')
+  text=text.replace(/ ([\)])/g, '$1')
+  text=text.replace(/([\(]) /g, '$1')
+  $('#edit').html(text);
 
 });
