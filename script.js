43691e63f493f27c63bd2f62e548fc8db2fa0507
Joharaoc <lilinotes@gmail.com>
2020-04-21 14:08:09 +0200

    minor fix

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
+(function(){
 
-function lapoz(){
-  $('#info').toggle();
-  $('#app').toggle();
-}
+  if(window.location.hash==="#info") lapoz();
 
-var hash=window.location.hash;
-if(hash==="#info") lapoz();
+  $('a.extra-info').on('click', function(e){
+    lapoz();
+  });
 
-$('a.extra-info').on('click', function(e){
-  lapoz();
-});
+  $('#clear').on('click', function(e){
+    $('#edit').html("");
+  });
 
-$('#clear').on('click', function(e){
-  $('#edit').html("");
+  $('#replace').on('click', function(e){
+    $('#edit').html(formatText($('#edit')));
+  });
 
-});
-
-
-$('#replace').on('click', function(e){
-
-  var roots=$('#edit');
-
-  for (var i = 0; i < roots.length; i++) {
-    var root = $(roots[i])
-    root.append(root.find("div > div"))
+  function lapoz(){
+    $('#info').toggle();
+    $('#app').toggle();
   }
 
-  var html=$('#edit').html()
-  var text = html.replace(/<[^(br)(div)(p)][^>]+>/g, '');
-  text=text.replace(/<div[^>]+>/g, '<br>')
-  text=text.replace(/<p[^>]+>/g, '<br>')
-  text=text.replace(/<br>\s*<br>\s*<br>/gm, '<br><br>')
-  text=text.replace(/(&nbsp;)+/g, ' ')
-  text=text.replace(/ +/g, ' ')
-  text=text.replace(/-+/g, '-')
-  text=text.replace(/\.\.\.+/g, '...')
-  text=text.replace(/ ([\.\?\!\,\:])/g, '$1')
-  text=text.replace(/([\.\?\!\,\:])(?! )/g, '$1 ')
-  text=text.replace(/\. \. \.+/g, '...')
-  text=text.replace(/\? \!/g, '?!')
-  text=text.replace(/\! \?/g, '!?')
-  text=text.replace(/\! \! \!/g, '!!!')
-  text=text.replace(/ ([\)])/g, '$1')
-  text=text.replace(/([\(]) /g, '$1')
-  $('#edit').html(text);
+  function formatText(el){
 
-});
+    const replaceRules=[
+      {re:/<[^(br)(div)(p)][^>]+>/g, to: ''},
+      {re:/<div[^>]*>/g, to: '<br>'},
+      {re:/<p[^>]*>/g, to:'<br>'},
+      {re:/<br>\s*<br>\s*<br>/gm, to:'<br><br>'},
+      {re:/(&nbsp; *)+/g, to:' '},
+      {re:/ +/g, to:' '},
+      {re:/-+/g, to:'-'},
+      {re:/\.\.\.+/g, to:'...'},
+      {re:/ ([\.\?\!\,\:])/g, to:'$1'},
+      {re:/([\.\?\!\,\:])(?! )/g, to:'$1'},
+      {re:/\. \. \.+/g, to:'...'},
+      {re:/\? \!/g, to:'?!'},
+      {re:/\! \?/g, to:'!?'},
+      {re:/\! \! \!/g, to:'$1'},
+      {re:/([\(]) /g, to:'$1'},
+    ];
+
+    let html=el.html()
+
+    for (let i = 0; i < replaceRules.length; i++) {
+      let rule=replaceRules[i];
+      html=html.replace(rule.re, rule.to);
+    }
+    return html;
+  }
+})();
